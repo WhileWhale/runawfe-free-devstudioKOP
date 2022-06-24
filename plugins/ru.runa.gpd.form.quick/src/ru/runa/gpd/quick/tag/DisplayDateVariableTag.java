efd062ea1e858e58b391edfa6a95aaeb933a8dab
Михаил Копорев <196397@edu.fa.ru>
2022-06-24 13:55:07 +0300

    Added DisplayDateVariableTag.java

---
+++ plugins/ru.runa.gpd.form.quick/src/ru/runa/gpd/quick/tag/DisplayDateVariableTag.java
@@ -0,0 +1,26 @@
+package ru.runa.gpd.quick.tag;
+
+import java.time.LocalDate;
+import ru.runa.wfe.var.dto.WfVariable;
+import freemarker.template.TemplateModelException;
+
+public class DisplayVariableTag extends FreemarkerTagGpdWrap {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected Object executeTag() throws TemplateModelException {
+        String variableName = getParameterAs(String.class, 0);
+        boolean componentView = getParameterAs(boolean.class, 1);
+        WfVariable variable = variableProvider.getVariableNotNull(variableName);
+        LocalDate date = LocalDate.now();
+        if (componentView) {
+            return ViewUtil.getComponentOutput(user, variableName, variable.getDefinition().getFormatClassName(), variable.getValue());
+        } else {
+            String html = "<span class=\"displayVariable\">";
+            html += ViewUtil.getOutput(user, webHelper, variableProvider.getProcessId(), variable);
+            html += date;
+            html += "</span>";
+            return html;
+        }
+    }
+}
